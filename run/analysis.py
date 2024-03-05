bab37275ad18b19a96070c3d2f885ecc55d71d70
shuoli90 <lishuo1@seas.upenn.edu>
2024-03-05 15:18:15 -0500

    plot together

--- run/analysis.py
+++ run/analysis.py
@@ -20,33 +20,30 @@ if __name__ == '__main__':
     parser.add_argument('--model', type=str, default='meta-llama/Llama-2-7b-chat-hf')
     parser.add_argument('--temperature', type=float, default=0.6)
     parser.add_argument('--dataset', type=str, default='triviaqa')
-    parser.add_argument('--affinity_mode', type=str, default='disagreement')
-    parser.add_argument('--method', type=str, default='whitebox')
     parser.add_argument('--mode', type=str, default='rougeL')
     args = parser.parse_args()
 
     # list all csv files in the root directory
     print(f"Loading files from {args.root_dir}")
     file_names = [file for file in os.listdir(args.root_dir) if file.endswith('.json')]
-
-    # setup generation correctness score
-    if args.correctness in ['rouge', 'bleu', 'meteor']:
-        SCORE = correctness.Score(metric_name=args.correctness, mode=args.mode)
-    elif args.correctness in ['bert_similarity']:
-        SCORE = correctness.BertSimilarity()
-    elif args.correctness in ['chatgpt']:
-        SCORE = correctness.ChatgptCorrectness()
-    else:
-        raise ValueError(f"Correctness metric {args.correctness} not found")
-
     model = args.model.split('/')[-1]
-    collected_file = '_'.join([model, args.dataset, str(args.temperature)]) + '.json'
-    collected = json.load(open(os.path.join('../collected', collected_file)))
-
     # compute the correctness score
     if os.path.exists(f'../tmp/{model}_{args.dataset}_{args.temperature}_{args.correctness}.json'):
         scores = json.load(open(f'../tmp/{model}_{args.dataset}_{args.temperature}_{args.correctness}.json'))
     else:
+        model = args.model.split('/')[-1]
+        collected_file = '_'.join([model, args.dataset, str(args.temperature)]) + '.json'
+        collected = json.load(open(os.path.join('../collected', collected_file)))
+         # setup generation correctness score
+        if args.correctness in ['rouge', 'bleu', 'meteor']:
+            SCORE = correctness.Score(metric_name=args.correctness, mode=args.mode)
+        elif args.correctness in ['bert_similarity']:
+            SCORE = correctness.BertSimilarity()
+        elif args.correctness in ['chatgpt']:
+            SCORE = correctness.ChatgptCorrectness()
+        else:
+            raise ValueError(f"Correctness metric {args.correctness} not found")
+    
         from multiprocessing import Manager
         if args.correctness in ['rouge', 'bleu', 'meteor']:
             def compute_scores(col, base, scores):
@@ -119,124 +116,122 @@ if __name__ == '__main__':
             json.dump(scores, f)
         exit(0)
     scores = pd.DataFrame(scores).dropna(axis=0)
-    
-    if args.method == 'whitebox':
-        affinity_mode = 'none'
-    else:
-        affinity_mode = args.affinity_mode
-    file_name = "_".join(['calibrate', model, args.dataset, str(args.temperature), affinity_mode, args.method]) + '.json'
-    file_name = os.path.join(args.root_dir, file_name)
 
     model = args.model.split('/')[-1]
     dataset = args.dataset
-    method = args.method
-
-    print('loading', os.path.join(args.root_dir, file_name))
-    data = json.load(open(file_name))[:len(scores)]
-
-    results = [] 
-    for idx, row in tqdm(enumerate(data), total=len(data)):
+    file_names = []
+    for method in ['whitebox', 'blackbox']:
+        if method == 'whitebox':
+            affinity_mode = 'none'
+            file_name = "_".join(['calibrate', model, dataset, str(args.temperature), affinity_mode, 'whitebox']) + '.json'
+            file_names.append(file_name)
+        else:
+            for affinity_mode in ['disagreement', 'agreement']:
+                file_name = "_".join(['calibrate', model, dataset, str(args.temperature), affinity_mode, 'blackbox']) + '.json'
+                file_names.append(file_name)
+    data_whitebox = json.load(open(os.path.join(args.root_dir, file_names[0])))
+    data_blackbox_disagreement = json.load(open(os.path.join(args.root_dir, file_names[1])))
+    data_blackbox_agreement = json.load(open(os.path.join(args.root_dir, file_names[2])))
+    results = []
+    for idx, (row_whitebox, row_blackbox_disagreement, row_blackbox_agreement) in tqdm(enumerate(zip(data_whitebox, data_blackbox_disagreement, data_blackbox_agreement)), total=len(data_whitebox)):
         result = {'model':model, 'dataset':dataset, 'method':method, 
                     'metric':args.correctness, 'mode':args.mode}
-        if method == 'blackbox':
-            result['ecc_u'] = row['ecc_u']
-            result['ecc_c'] = row['ecc_c']
-            result['degree_u'] = row['degree_u']
-            result['degree_c'] = row['degree_c']
-            result['spectral_u'] = row['spectral_u']
-        elif method == 'whitebox':
-            normalized_nll = row['normalized_nll']
-            unnormalized_nll = row['unnormalized_nll']
-            entropy_normalized = row['entropy_normalized']
-            entropy_unnormalized = row['entropy_unnormalized']
-            result['normalized_nll_all'] = normalized_nll
-            result['unnormalized_nll_all'] = unnormalized_nll
-            result['normalized_nll_greedy'] = np.min(normalized_nll)
-            result['unnormalized_nll_greedy'] = np.min(unnormalized_nll)
-            result['entropy_normalized'] = entropy_normalized
-            result['entropy_unnormalized'] = entropy_unnormalized  
+        
+        result['ecc_c_disagreement'] = row_blackbox_disagreement['ecc_c']
+        result['degree_c_disagreement'] = row_blackbox_disagreement['degree_c']
+        result['ecc_u_disagreement'] = row_blackbox_disagreement['ecc_u']
+        result['degree_u_disagreement'] = row_blackbox_disagreement['degree_u']
+        result['spectral_u_disagreement'] = row_blackbox_disagreement['spectral_u']
+
+        result['ecc_c_agreement'] = row_blackbox_agreement['ecc_c']
+        result['degree_c_agreement'] = row_blackbox_agreement['degree_c']
+        result['ecc_u_agreement'] = row_blackbox_agreement['ecc_u']
+        result['degree_u_agreement'] = row_blackbox_agreement['degree_u']
+        result['spectral_u_agreement'] = row_blackbox_agreement['spectral_u']
+
+        result['entropy_normalized'] = row_whitebox['entropy_normalized']
+        result['entropy_unnormalized'] = row_whitebox['entropy_unnormalized']
+        result['normalized_nll_all'] = row_whitebox['normalized_nll']
+        result['unnormalized_nll_all'] = row_whitebox['unnormalized_nll']
+        result['normalized_nll_greedy'] = np.min(row_whitebox['normalized_nll'])
+        result['unnormalized_nll_greedy'] = np.min(row_whitebox['unnormalized_nll'])
+
         # select scores with the same index
         score = scores[scores['id'] == idx]
         result['normalized_score_all'] = score.iloc[0]['normalized_score']
         result['unnormalized_score_all'] = score.iloc[0]['unnormalized_score']
-        if method == 'whitebox':
-            normalized_min_index = np.argmin(result['normalized_nll_all'])
-            unnormalized_min_index = np.argmin(result['unnormalized_nll_all'])
-            result['normalized_score_greedy'] = result['normalized_score_all'][normalized_min_index]
-            result['unnormalized_score_greedy'] = result['unnormalized_score_all'][unnormalized_min_index]
+        normalized_min_index = np.argmin(result['normalized_nll_all'])
+        unnormalized_min_index = np.argmin(result['unnormalized_nll_all'])
+        result['normalized_score_greedy'] = result['normalized_score_all'][normalized_min_index]
+        result['unnormalized_score_greedy'] = result['unnormalized_score_all'][unnormalized_min_index]
         results.append(result)
     df = pd.DataFrame(results).dropna(axis=0)
     scores = pd.DataFrame(scores).dropna(axis=0)
     # concatenate the scores
     df = pd.concat([df, scores], axis=1)
 
+    uncertainty_indicators = ['ecc_u_disagreement', 'degree_u_disagreement', 'spectral_u_disagreement',
+                              'ecc_u_agreement', 'degree_u_agreement', 'spectral_u_agreement',
+                              'normalized_nll_greedy', 'unnormalized_nll_greedy', 'entropy_normalized', 'entropy_unnormalized']
+    confidence_indicators = ['ecc_c_disagreement', 'degree_c_disagreement', 'ecc_c_agreement', 'degree_c_agreement', 
+                             'normalized_nll_all', 'unnormalized_nll_all']
+    indicators = uncertainty_indicators + confidence_indicators
+
     try: 
-        path = os.path.join(args.root_dir, f"{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}")
+        path = os.path.join(args.root_dir, f"{model}_{dataset}_{args.correctness}_{args.temperature}")
         os.makedirs(path, exist_ok = True) 
     except OSError as error: 
         print("Directory '%s' can not be created" % path) 
 
-    if method == 'whitebox':
-        fig, ax = plt.subplots()
-        # correctness_score = df['score'].to_numpy()
-        correctness_score = df['normalized_score_greedy'].to_numpy()
-        for indicator in ['normalized_nll_greedy', 'unnormalized_nll_greedy', 'entropy_normalized', 'entropy_unnormalized']:
-            confidence = -df[indicator].to_numpy()
-            thresholds = np.linspace(np.min(correctness_score)+epsilon, np.max(correctness_score)-epsilon, 10)
-            ax = make_plots.AUROC_vs_Correctness(correctness_score, confidence, thresholds, ax=ax, label=indicator)
-        ax.set_title(f'AUROC vs Correctness Threshold {model} {dataset} {method} {args.correctness}')
-        ax.grid()
-        ax.figure.savefig(f'{path}/auroc_vs_correctness_{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}.png')
-
-    if method == 'whitebox':
-        indicators = ['normalized_nll_all', 'unnormalized_nll_all']
-    else:
-        indicators = ['ecc_c', 'degree_c']
+    fig, ax = plt.subplots()
+    # correctness_score = df['score'].to_numpy()
+    correctness_score = df['normalized_score_greedy'].to_numpy()
+    for indicator in uncertainty_indicators:
+        confidence = -df[indicator].to_numpy()
+        thresholds = np.linspace(np.min(correctness_score)+epsilon, np.max(correctness_score)-epsilon, 10)
+        ax = make_plots.AUROC_vs_Correctness(correctness_score, confidence, thresholds, ax=ax, label=indicator)
+    ax.set_title(f'AUROC vs Correctness Threshold {model} {dataset} {method} {args.correctness}')
+    ax.grid()
+    ax.figure.savefig(f'{path}/auroc_vs_correctness_{model}_{dataset}_{args.correctness}.png')
 
     correctness_scores = np.stack(df['normalized_score_all'])
     fig, ax = plt.subplots()
-    for indicator in indicators:
-        if method == 'whitebox':
-            confidence = -np.stack(df[indicator])
-        else:
-            confidence = np.stack(df[indicator])
-        
+    for indicator in confidence_indicators:
+        confidence = np.stack(df[indicator]) if 'agreement' in indicator else -np.stack(df[indicator])
         min_val = np.max(np.min(correctness_scores, axis=0))
         max_val = np.min(np.max(correctness_scores, axis=0))
         thresholds = np.linspace(min_val+epsilon, max_val-epsilon, 10)
         ax = make_plots.AUROC_vs_Correctness_average(correctness_scores, confidence, thresholds, ax=ax, label=indicator)
-    ax.set_title(f'AUROC vs Correctness Threshold {model} {dataset} {method} {args.correctness}')
+    ax.set_title(f'AUROC vs Correctness Threshold {model} {dataset} {args.correctness}')
     ax.grid()
-    ax.figure.savefig(f'{path}/auroc_vs_correctness_average_{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}.png')
+    ax.figure.savefig(f'{path}/auroc_vs_correctness_average_{model}_{dataset}_{args.correctness}.png')
 
     # correctness_score = df['normalized_score_greedy'].to_numpy()
-    correctness_scores = np.stack(df['normalized_score_all']).flatten()
-    for indicator in indicators:
-        fig, ax = plt.subplots()
-        if method == 'whitebox':
-            confidence = -np.stack(df[indicator]).flatten()
-        else:
-            confidence = np.stack(df[indicator]).flatten()
-        # plot a scatter plot of correctness score vs uncertainty
-        ax.scatter(correctness_scores, confidence)
-        ax.set_title(f'Correctness score vs {indicator}')
-        ax.set_xlabel('Correctness score')
-        ax.set_ylabel(f'{indicator}')
-        plt.grid()
-        plt.savefig(f'{path}/erce_scatter_{model}_{dataset}_{affinity_mode}_{indicator}_{args.correctness}.png')
+    # correctness_scores = np.stack(df['normalized_score_all']).flatten()
+    # for indicator in confidence_indicators:
+    #     fig, ax = plt.subplots()
+    #     if method == 'whitebox':
+    #         confidence = -np.stack(df[indicator]).flatten()
+    #     else:
+    #         confidence = np.stack(df[indicator]).flatten()
+    #     # plot a scatter plot of correctness score vs uncertainty
+    #     ax.scatter(correctness_scores, confidence)
+    #     ax.set_title(f'Correctness score vs {indicator}')
+    #     ax.set_xlabel('Correctness score')
+    #     ax.set_ylabel(f'{indicator}')
+    #     plt.grid()
+    #     plt.savefig(f'{path}/erce_scatter_{model}_{dataset}_{indicator}_{args.correctness}.png')
 
-    if method == 'whitebox':
-        indicators = ['normalized_nll_greedy', 'unnormalized_nll_greedy', 'entropy_normalized', 'entropy_unnormalized']
-    else:
-        indicators = ['ecc_u', 'degree_u', 'spectral_u']
     fig, ax = plt.subplots()
-    ax.violinplot(df[indicators],
+    ax.violinplot(df[uncertainty_indicators],
                 showmeans=False,
                 showmedians=True)
     ax.set_title('Uncertainty value distribution')
-    ax.set_xticks([y+1 for y in range(len(indicators))], labels=indicators)
+    ax.set_xticks([y+1 for y in range(len(uncertainty_indicators))], labels=uncertainty_indicators)
     plt.grid()
-    plt.savefig(f'{path}/confidence_histogram_{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}.png')
+    # rotate xlable by 45 degree
+    plt.xticks(rotation=45)
+    plt.savefig(f'{path}/uncertainty_histogram_{model}_{dataset}_{args.correctness}.png')
 
     # plot the histogram of correctness score
     fig, ax = plt.subplots()
@@ -245,52 +240,44 @@ if __name__ == '__main__':
     ax.set_xlabel('Correctness score')
     ax.set_ylabel('Frequency')
     plt.grid()
-    plt.savefig(f'{path}/correctness_histogram_{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}.png')
+    plt.savefig(f'{path}/correctness_histogram_{model}_{dataset}_{args.correctness}.png')
 
-    if method == 'whitebox':
+    correctness_scores = np.stack(df['normalized_score_greedy']).flatten()
+    for indicator in uncertainty_indicators:
         fig, ax = plt.subplots()
-        confidence = -np.stack(df['unnormalized_nll_all']).flatten() if args.correctness != 'bert_similarity' else np.stack(df['unnormalized_nll_all']).flatten()
-        ax = make_plots.histogram(correctness_scores, confidence, fig, ax)
-        plt.savefig(f'{path}/erce_{model}_{dataset}_{affinity_mode}_unnormalized_nll_all_{args.correctness}.png')
+        uncertainty = df[indicator].to_numpy()
+        ax = make_plots.histogram_alternative(correctness=correctness_scores, uncertainties=uncertainty, ax=ax, num_bins=50)
+        ax.set_title(f'{indicator} distribution')
+        ax.set_xlabel(f'{indicator}')
+        ax.set_ylabel('Frequency')
+        plt.grid()
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig(f'{path}/{indicator}_erce_{model}_{dataset}_{args.correctness}.png')
+    
+    # correctness_scores = np.stack(df['normalized_score_all']).flatten()
+    # for indicator in confidence_indicators:
+    #     fig, ax = plt.subplots()
+    #     confidence = -np.stack(df[indicator]) if 'agreement' in indicator else np.stack(df[indicator])
+    #     ax = make_plots.histogram_alternative(correctness=correctness_scores, uncertainties=confidence, ax=ax, num_bins=50)
+    #     ax.set_title(f'{indicator} distribution')
+    #     ax.set_xlabel(f'{indicator}')
+    #     ax.set_ylabel('Frequency')
+    #     plt.grid()
+    #     plt.savefig(f'{path}/{indicator}_confidence_histogram_{model}_{dataset}_{args.correctness}.png')
 
-        fig, ax = plt.subplots()
-        threshold = 0.5
-        y_true = correctness_scores >= threshold
-        # plot roc curve
-        fpr, tpr, _ = roc_curve(y_true, -confidence)
-        ax.plot(fpr, tpr, label='ROC curve')
-        ax.plot([0, 1], [0, 1], 'k--', label='Random')
-        ax.set_xlabel('False Positive Rate')
-        ax.set_ylabel('True Positive Rate')
-        ax.set_title('ROC curve')
-        ax.legend()
-        ax.grid()
-        plt.savefig(f'{path}/roc_curve_{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}_entropy.png')
-    else:
-        fig, ax = plt.subplots()
-        confidence = np.stack(df['degree_c']).flatten()
-        ax = make_plots.histogram(correctness_scores, confidence, fig, ax)
-        plt.savefig(f'{path}/erce_{model}_{dataset}_{affinity_mode}_ecc_u_{args.correctness}.png')
-
-        fig, ax = plt.subplots()
-        threshold = 0.5
-        y_true = correctness_scores >= threshold
-        # plot roc curve
-        fpr, tpr, _ = roc_curve(y_true, confidence)
-        ax.plot(fpr, tpr, label='ROC curve')
-        ax.plot([0, 1], [0, 1], 'k--', label='Random')
-        ax.set_xlabel('False Positive Rate')
-        ax.set_ylabel('True Positive Rate')
-        ax.set_title('ROC curve')
-        ax.legend()
-        ax.grid()
-        plt.savefig(f'{path}/roc_curve_{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}_degree.png')
-
+    confidence = df['unnormalized_entropy'].to_numpy()
+    correctness_scores = df['normalized_score_greedy'].to_numpy()
     fig, ax = plt.subplots()
-    ax.violinplot(df[indicators],
-                showmeans=False,
-                showmedians=True)
-    ax.set_title('Uncertainty value distribution')
-    ax.set_xticks([y+1 for y in range(len(indicators))], labels=indicators)
-    plt.grid()
-    plt.savefig(f'{path}/confidence_histogram_{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}.png')
+    threshold = 0.5
+    y_true = correctness_scores >= threshold
+    # plot roc curve
+    fpr, tpr, _ = roc_curve(y_true, confidence)
+    ax.plot(fpr, tpr, label='ROC curve')
+    ax.plot([0, 1], [0, 1], 'k--', label='Random')
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('ROC curve')
+    ax.legend()
+    ax.grid()
+    plt.savefig(f'{path}/roc_curve_{model}_{dataset}_{affinity_mode}_{method}_{args.correctness}_entropy.png')
