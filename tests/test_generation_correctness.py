c47f6749566230a026e3212a5642b76563ee05cf
shuoli90 <lishuo1@seas.upenn.edu>
2024-03-05 18:26:26 -0500

    update tests

--- tests/test_generation_correctness.py
+++ tests/test_generation_correctness.py
@@ -5,14 +5,18 @@ import torch
 from metrics import correctness
 
 if __name__ == '__main__':
-    metric_names = ['rouge', 'bleu', 'meteor']
-    generations = ['J. Biden', 'Morning']
-    references = [['Joe Biden', 'President Joe Biden'], ['Joe Biden', 'President Joe Biden']]
-    for metric_name in metric_names:
-        if metric_name == 'rouge':
-            score = correctness.Score(metric_name=metric_name, mode='rouge1')
-        elif metric_name == 'bleu':
-            score = correctness.Score(metric_name=metric_name, mode='bleu')
-        elif metric_name == 'meteor':
-            score = correctness.Score(metric_name=metric_name, mode='meteor')
-        print(metric_name, score(generations, references))
+    # metric_names = ['rouge', 'bleu', 'meteor']
+    # generations = ['J. Biden', 'Morning']
+    # references = [['Joe Biden', 'President Joe Biden'], ['Joe Biden', 'President Joe Biden']]
+    # for metric_name in metric_names:
+    #     if metric_name == 'rouge':
+    #         score = correctness.Score(metric_name=metric_name, mode='rouge1')
+    #     elif metric_name == 'bleu':
+    #         score = correctness.Score(metric_name=metric_name, mode='bleu')
+    #     elif metric_name == 'meteor':
+    #         score = correctness.Score(metric_name=metric_name, mode='meteor')
+    #     print(metric_name, score(generations, references))
+    
+    chatgpt = correctness.ChatgptCorrectness()
+    prompt = 'who is the current president of the united states?'
+    score = chatgpt(prompt, reference='Joe Biden', generateds=['Joe Biden', 'trump'])
