e680028a56f1721dd2a9fde5579b8bda730d942a
shuoli90 <lishuo1@seas.upenn.edu>
2024-03-14 09:39:07 -0400

    update implementation

--- metrics/ranking.py
+++ metrics/ranking.py
@@ -1,4 +1,4 @@
 import utils.cd_diagram as cd
 
-def plot_cd_diagram(df, title, save_dir='../tmp', col1='indicator', col2='metric', col3='score'):
-    cd.draw_cd_diagram(df_perf=df, title=title, labels=True, save_dir=save_dir, col1=col1, col2=col2, col3=col3)
+def plot_cd_diagram(df, title, alpha=0.1, save_dir='../tmp', col1='indicator', col2='metric', col3='score'):
+    cd.draw_cd_diagram(df_perf=df, title=title, labels=True, save_dir=save_dir, col1=col1, col2=col2, col3=col3, alpha=alpha)
